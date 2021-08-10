0be9c0ab320387aa0b246b24527426dd0de02976
Garrido <86725591+Garrido10@users.noreply.github.com>
2021-08-09 22:15:53 -0300

    Implementando Tests.

--- blogPessoal/src/main/java/org/generation/blogPessoal/security/BasicSecurityConfig.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/security/BasicSecurityConfig.java
@@ -7,18 +7,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	private @Autowired UserDetailsService userDetailsService;
+	private @Autowired UserDetailsServiceImpl service;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService);
+		auth.userDetailsService(service);
 
 	}
 
@@ -29,7 +28,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
+				http.authorizeRequests()
 				.antMatchers("/usuarios/cadastrar").permitAll()
 				.antMatchers("/usuarios/logar").permitAll()
 				.anyRequest().authenticated()
--- blogPessoal/src/main/java/org/generation/blogPessoal/service/UsuarioService.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/service/UsuarioService.java
@@ -1,15 +1,19 @@
 package org.generation.blogPessoal.service;
 
 import java.nio.charset.Charset;
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.Optional;
-import org.apache.commons.codec.binary.Base64;
 
+import org.apache.commons.codec.binary.Base64;
 import org.generation.blogPessoal.model.Usuario;
 import org.generation.blogPessoal.model.UsuarioLogin;
 import org.generation.blogPessoal.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class UsuarioService {
@@ -18,6 +22,15 @@ public class UsuarioService {
 	private UsuarioRepository usuarioRepository;
 
 	public Optional<Usuario> cadastrarUsuario(Usuario usuario) {
+
+		if (usuarioRepository.findByUsuario(usuario.getUsuario()).isPresent())
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário já existe!", null);
+
+		int idade = Period.between(usuario.getDataNascimento(), LocalDate.now()).getYears();
+
+		if (idade < 18)
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário menor de 18 anos", null);
+
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
 		String senhaEncoder = encoder.encode(usuario.getSenha());
@@ -26,7 +39,32 @@ public class UsuarioService {
 		return Optional.of(usuarioRepository.save(usuario));
 	}
 
+	public Optional<Usuario> atualizarUsuario(Usuario usuario) {
+
+		if (usuarioRepository.findById(usuario.getId()).isPresent()) {
+
+			int idade = Period.between(usuario.getDataNascimento(), LocalDate.now()).getYears();
+
+			if (idade < 18)
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário menor de 18 anos", null);
+
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+			String senhaEncoder = encoder.encode(usuario.getSenha());
+			usuario.setSenha(senhaEncoder);
+
+			return Optional.of(usuarioRepository.save(usuario));
+
+		} else {
+
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuário não encontrado!", null);
+
+		}
+
+	}
+
 	public Optional<UsuarioLogin> logarUsuario(Optional<UsuarioLogin> usuarioLogin) {
+
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		Optional<Usuario> usuario = usuarioRepository.findByUsuario(usuarioLogin.get().getUsuario());
 
@@ -36,7 +74,7 @@ public class UsuarioService {
 				String auth = usuarioLogin.get().getUsuario() + ":" + usuarioLogin.get().getSenha();
 				byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
 				String authHeader = "Basic " + new String(encodedAuth);
-				
+
 				usuarioLogin.get().setId(usuario.get().getId());
 				usuarioLogin.get().setToken(authHeader);
 				usuarioLogin.get().setNome(usuario.get().getNome());
@@ -45,6 +83,6 @@ public class UsuarioService {
 				return usuarioLogin;
 			}
 		}
-		return null;
+		throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Usuário ou senha inválidos!", null);
 	}
 }
